a990f9f18bee0d382aba4ed489863df9fb2c66cd
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-07-16 14:09:13 +0200

    Adding preliminary profiles for English and Dutch.

--- individual/single_lang_extract.py
+++ individual/single_lang_extract.py
@@ -38,12 +38,12 @@ PROFILE_EN = {
     'm,'  : 'm̩',
     'n,'  : 'n̩',
     'l,'  : 'l̩',
-    'r*'  : '*', ###########
+    'r*'  : 'ɹ',
     'I'   : 'ɪ',
     'E'   : 'ɛ',
     '&'   : 'æ',
     'V'   : 'ʌ',
-    'O'   : 'ɔ', #### check, as all O
+    'O'   : 'ɒ',
     'U'   : 'ʊ',
     '@'   : 'ə',
     'i:'  : 'iː',
@@ -78,7 +78,7 @@ PROFILE_NL = {
     'm'   : 'm',
     'n'   : 'n',
     'l'   : 'l',
-    'r'   : 'ʀ', # check
+    'r'   : 'r',
     'f'   : 'f',
     'v'   : 'v',
     's'   : 's',
@@ -89,7 +89,7 @@ PROFILE_NL = {
     'x'   : 'x',
     'G'   : 'ɣ',
     'h'   : 'h',
-    'w'   : 'ʋ', # check
+    'w'   : 'ʋ',
     'dZ'  : 'dʒ',
     'i:'  : 'iː',
     'y:'  : 'yː',
@@ -101,13 +101,13 @@ PROFILE_NL = {
     'E'   : 'ɛ',
     'A'   : 'ɑ',
     'O'   : 'ɔ',
-    'U'   : 'ᵿ', # check if right symbol, should be horseshoe
+    'U'   : 'ʏ',
     'U:'  : 'œː', # confirm
     '@'   : 'ə',
     '&:'  : 'øː',
     'E:'  : 'ɛː',
     'i::' : 'iːː',
-    'O:'  : 'ɔː', # confirm symbol
+    'O:'  : 'ɔː',
     'y::' : 'yːː', 
     'EI' : 'ɛi',
     'UI' : 'œy',
@@ -154,24 +154,44 @@ def read_celex(filename, profile, ortho, phono):
     # get only orthography and disc
     transcr = defaultdict(list)
     for entry in data:
-#        print(entry[ortho], entry[phono])
         transcr[entry[ortho]].append(apply_profile(entry[phono], profile))
+#        if "U:" in entry[phono]:
+#            print(entry[ortho], entry[phono], apply_profile(entry[phono], profile))
+#            input()
+
     
     return transcr
                 
 
-def main(ref, lang_id):
+def main(ref, lang_id, output_file, lang_name):
     with open("../cldf/forms.csv") as csvfile:
         reader = csv.DictReader(csvfile)
         data = [
             row['Form'] for row in reader
             if row['Language_ID'] == lang_id
         ]
-        
-    for form in data:
-        transcr = list(set(ref.get(form, [])))
     
-        print(form, transcr)
+    transcr = {}
+    for form in data:
+        # clean form for transcription
+        clean_form = form.replace("(1)", "")
+        clean_form = clean_form.replace("(2)", "")
+        clean_form = clean_form.strip()
+    
+        transcr[form] = list(set(ref.get(clean_form, ['???'])))
+
+    # output
+    with open(output_file, 'w') as handler:
+        handler.write("Grapheme\tIPA\tLanguages\n")
+
+        for form in sorted(transcr):
+            output_form = "^%s$" % form
+            output_ipa = " ".join(transcr[form])
+            buf = [output_form, output_ipa, lang_name]
+        
+            handler.write("\t".join(buf))
+            handler.write("\n")
+
 
 if __name__ == "__main__":
     # Read CELEX data
@@ -179,4 +199,5 @@ if __name__ == "__main__":
     dutch = read_celex('dutch/dpw/dpw.cd', PROFILE_NL, ortho=1, phono=6)
 
     # dutch 12, english 13
-    main(english, "13")
+    main(english, "13", "stan1293.profile.tsv", "English")
+    main(dutch, "12", "dutc1256.profile.tsv", "Dutch")
