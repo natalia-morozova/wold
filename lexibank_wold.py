28b9d73cb273b140962b260e4e7327b133891d2e
Tiago Tresoldi <tresoldi@shh.mpg.de>
2019-09-05 18:12:09 +0200

    Minor refactoring.

--- lexibank_wold.py
+++ lexibank_wold.py
@@ -9,6 +9,7 @@ import re
 from segments import Profile, Tokenizer
 import unicodedata
 
+
 @attr.s
 class WOLDLexeme(Lexeme):
     Word_ID = attr.ib(default=None)
@@ -30,28 +31,26 @@ class WOLDLexeme(Lexeme):
 
 
 class Dataset(clld.CLLD):
-    __cldf_url__ = (
-        "http://cdstar.shh.mpg.de/bitstreams/EAEA0-92F4-126F-089F-0/wold_dataset.cldf.zip"
-    )
+    __cldf_url__ = "http://cdstar.shh.mpg.de/bitstreams/EAEA0-92F4-126F-089F-0/wold_dataset.cldf.zip"
     dir = Path(__file__).parent
     id = "wold"
     lexeme_class = WOLDLexeme
-    
+
     doc_tokenizers = None
 
     def tokenizer(self, form, doculect):
-        tok_form = '^%s$' % form
+        tok_form = "^%s$" % form
         tokens = self.doc_tokenizers[doculect](
-            unicodedata.normalize('NFC', tok_form),
-            column="IPA")
-            
+            unicodedata.normalize("NFC", tok_form), column="IPA"
+        )
+
         return tokens.split()
-        
+
     def clean_form(self, form):
         # we cannot use clldutils.text.strip_brackets(), as brackets most
         # of the time contain phonological material
         form = re.sub("\(\d\)", "", form)
-        
+
         # To split with clldutils.text, we'd need a regex pattern, as
         # we need to include the spaces around the tilde...
         form = form.split(",")[0]
@@ -64,22 +63,27 @@ class Dataset(clld.CLLD):
         # doculect ids (here, glottocodes), and build the appropriate
         # tokenizers
         profile_files = sorted(glob.glob(str(self.dir / "etc" / "*.prof")))
-        doculect_codes = [os.path.splitext(os.path.basename(pf))[0] for pf in profile_files]
+        doculect_codes = [
+            os.path.splitext(os.path.basename(pf))[0] for pf in profile_files
+        ]
 
         self.doc_tokenizers = {
-            doculect: Tokenizer(profile=Profile.from_file(pf, form='NFC'),
-            errors_replace=lambda c: '<{0}>'.format(c))
+            doculect: Tokenizer(
+                profile=Profile.from_file(pf, form="NFC"),
+                errors_replace=lambda c: "<{0}>".format(c),
+            )
             for pf, doculect in zip(profile_files, doculect_codes)
         }
 
         # Cache the Concepticon IDs
         concepticon = {
-            x.attributes["wold_id"]: x.concepticon_id for x in self.conceptlist.concepts.values()
+            x.attributes["wold_id"]: x.concepticon_id
+            for x in self.conceptlist.concepts.values()
         }
 
         # cache the field names for CLDF output
         fields = self.lexeme_class.fieldnames()
-        
+
         # Write data to CLDF
         with self.cldf as ds:
             vocab_ids = [v["ID"] for v in self.original_cldf["contributions.csv"]]
@@ -94,29 +98,35 @@ class Dataset(clld.CLLD):
                 if gc == "tzot1264":
                     gc, iso = "tzot1259", "tzo"
                 if row["ID"] in vocab_ids:
-                    ds.add_language(ID=row["ID"], Name=row["Name"], Glottocode=gc, ISO639P3code=iso)
+                    ds.add_language(
+                        ID=row["ID"], Name=row["Name"], Glottocode=gc, ISO639P3code=iso
+                    )
 
                 # Add to map only those which are receivers
-                if int(row['ID']) <= 41:
-                    lang_map[row['ID']]= gc
+                if int(row["ID"]) <= 41:
+                    lang_map[row["ID"]] = gc
 
             # add parameters
             for row in self.original_cldf["ParameterTable"]:
                 ds.add_concept(
-                    ID=row["ID"], Name=row.pop("Name"), Concepticon_ID=concepticon.get(row["ID"])
+                    ID=row["ID"],
+                    Name=row.pop("Name"),
+                    Concepticon_ID=concepticon.get(row["ID"]),
                 )
 
             # Being explicit on what we are adding
             for row in self.original_cldf["FormTable"]:
                 if row["Language_ID"] in vocab_ids:
                     # Copy the raw Form to Value, clean form, and tokenize
-                    row['Value'] = row['Form']
-                    row['Form'] = self.clean_form(row['Form'])
-                    row['Segments'] = self.tokenizer(row['Form'],
-                            lang_map[row['Language_ID']])
-                
-                    # Note: We count words marked as "probably borrowed" as loans.
-                    row['Loan'] = float(row["BorrowedScore"]) > 0.6
-                 
-                    ds.add_form_with_segments(**{k: v for k, v in row.items() if k in fields})            
+                    row["Value"] = row["Form"]
+                    row["Form"] = self.clean_form(row["Form"])
+                    row["Segments"] = self.tokenizer(
+                        row["Form"], lang_map[row["Language_ID"]]
+                    )
 
+                    # Note: We count words marked as "probably borrowed" as loans.
+                    row["Loan"] = float(row["BorrowedScore"]) > 0.6
+
+                    ds.add_form_with_segments(
+                        **{k: v for k, v in row.items() if k in fields}
+                    )
